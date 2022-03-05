2edf8754446b274e864f033f5b714b73e38b9830
DevOFVictory <DevOFVictory@gmail.com>
2022-03-05 14:01:41 +0100

    Started login view

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,59 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+<Page>
+    <ActionBar backgroundColor="#7c149c">
+        <Label text="PassX" fontSize="32px" horizontalAlignment="center"/>
+    </ActionBar>
+    
+    <Gridlayout backgroundColor="#a621cf" rows="*" columns="*">
+        <Stacklayout class="mainContainer">
+            <Label text="PassX" fontSize="24px" color="white" horizontalAlignment="center"/>
+            <Label text="A safe place for all your passwords!" fontSize="16px" color="white" horizontalAlignment="center"/>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+
+        <Stacklayout class="inputbox">
+            <Label text="Username" class="inputs label"/>
+            <TextField hint="Enter Username" autocorrect="false" autocapitalizationType="none" class="inputs"/>
+
+            <Label text="Password" class="inputs label password"/>
+            <TextField hint="Enter Password" secure="true" autocorrect="false" autocapitalizationType="none" class="inputs"/>
+            <Label text="Create a new account" horizontalAlignment="right" paddingRight="15" fontSize="14" color="rgb(120, 120, 120)"/>
+        </Stacklayout>
+
+        </Stacklayout>
+    </Gridlayout>
+</Page>
+    
 </template>
 
 <script>
   export default {
     computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
     }
   };
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+.mainContainer {
+    background-color: rgb(0, 0, 0, 0.5);
+    width: 90%;
+    height: 90%;
+}
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
+.inputs {
+    color: white;
+    font-size: 14;
+
+    &.password {
+        margin-top: 20;
     }
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
+    &.label {
+        padding-left: 15;
     }
+}
+
+.inputbox {
+    margin-top: 50;
+}
+
 </style>
